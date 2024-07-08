d9d3ecc5a337a87242e94b7ad2076ed8dfbeb3a4
karthisana-official <karthisana.official@gmail.com>
2024-07-08 13:25:17 +0530

    New code added

--- main.tf
+++ main.tf
@@ -1,4 +1,29 @@
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-  tags = merge({ "resourcename" = "${local.name}" }, local.tags)
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "vulnerable_bucket" {
+  bucket = "my-vulnerable-bucket"
+  acl    = "public-read"  # This makes the bucket publicly accessible
+
+  tags = {
+    Name        = "My Vulnerable S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_policy" "vulnerable_bucket_policy" {
+  bucket = aws_s3_bucket.vulnerable_bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.vulnerable_bucket.arn}/*"
+      }
+    ]
+  })
 }
