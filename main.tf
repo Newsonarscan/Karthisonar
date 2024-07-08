f608f68c837866df36868a963d9d3753f3e267cc
karthisana-official <karthisana.official@gmail.com>
2024-07-08 14:04:29 +0530

    New

--- main.tf
+++ main.tf
@@ -2,27 +2,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "vulnerable_bucket" {
-  bucket = "my-vulnerable-bucket"
-  acl    = "public-read"  # This makes the bucket publicly accessible
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-secure-bucket"
+  acl    = "private"  # Make the bucket private
 
   tags = {
-    Name        = "My Vulnerable S3 Bucket"
-    Environment = "Dev"
+    Name        = "My Secure S3 Bucket"
+    Environment = "Prod"
   }
 }
 
-resource "aws_s3_bucket_policy" "vulnerable_bucket_policy" {
-  bucket = aws_s3_bucket.vulnerable_bucket.bucket
+resource "aws_s3_bucket_policy" "secure_bucket_policy" {
+  bucket = aws_s3_bucket.secure_bucket.bucket
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
-        Principal = "*"
+        Principal = {
+          AWS = "arn:aws:iam::123456789012:role/MyTrustedRole"  # Restrict to specific AWS principal
+        }
         Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.vulnerable_bucket.arn}/*"
+        Resource = "${aws_s3_bucket.secure_bucket.arn}/*"
       }
     ]
   })
